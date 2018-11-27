fccbd994b2c5ad2688449fdebf6387953ebafff5
fredericogon <fredericogon@gmail.com>
2018-11-27 17:25:52 -0200

    aula51

--- aula51/src/br/com/cursojava/contatos/ControllerContato.java
+++ aula51/src/br/com/cursojava/contatos/ControllerContato.java
@@ -2,11 +2,15 @@ package br.com.cursojava.contatos;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 @Path("contatos")
 public class ControllerContato {
@@ -22,9 +26,27 @@ public class ControllerContato {
 	@GET
 	@Path("/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Contato buscarContatoPorId(@PathParam("id") Integer id){
+	public Response buscarContatoPorId(@PathParam("id") Integer id){
 		Contato contato = repos.buscarPorId(id);
-		return contato;
+		if(contato != null){
+			return Response.ok(contato).build();
+		}else{
+			return Response.status(Status.NOT_FOUND).build();
+		}
+
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response inserir(Contato contato){
+		if(contato != null && contato.getId() == null){
+			boolean result = repos.salvar(contato);
+			if (result){
+				return Response.status(Status.CREATED).build();
+			}
+		}
+		return Response.status(Status.NOT_ACCEPTABLE).build();
 	}
 
 }
